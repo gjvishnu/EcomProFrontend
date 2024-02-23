8ecec62e7261fdbecc8931dd5387b31f6f83ba15
gjvishnu <gjvishnu24@gmail.com>
2024-02-24 01:35:17 +0530

    third commit

--- src/collections.js
+++ src/collections.js
@@ -3,10 +3,10 @@ import { Fragment } from "react";
 export function Collection() {
   return (
     <Fragment>
-      <div className="collectionMain container mt-3 r">
-        <div className="row">
-          <div className="col-12 col-lg-6   d-flex justify-content-end">
-            <div className="collection_left">
+      <div className="collectionMain container mt-3 r ">
+        <div className="row collectionLg">
+          <div className="col-12 col-lg-6 col-md-6  d-flex justify-content-end">
+            <div className="collection_left d-flex align-items-end justify-content-end">
               <div className="p-2 pt-4 ps-3">
                 <p className="mb-0">HOT LIST</p>
                 <h4 className="mb-0">Womens Collection</h4>
@@ -14,7 +14,7 @@ export function Collection() {
               </div>
             </div>
           </div>
-          <div className="col-12 col-lg-6  ">
+          <div className="col-12 col-lg-6 col-md-6  ">
             <div className="rightOne">
               <div className="p-2 pt-4 ps-3">
                 <p className="mb-0">HOT LIST</p>
@@ -36,12 +36,31 @@ export function Collection() {
         </div>
 
         {/* mobile responsive  */}
-        {/* <div className="row d-flex justify-content-center">
-            <div className="col-12 collection_left"></div>
-            <div className="col-12 collection_left "></div>
-            <div className="col-12collection_left "></div>
-            <div className="col-12 collection_left"></div>
-          </div> */}
+        <div className="container collectionRes">
+          <div className="row d-flex justify-content-center">
+            <div className="col-12 resCollectOne">
+              <div className="p-2 pt-4 ps-3">
+                <p className="mb-0 text-white">HOT LIST</p>
+                <h4 className="mb-0 text-white">Womens Collection</h4>
+                <p className="text-white">Shop now</p>
+              </div>
+            </div>
+            <div className="col-12 my-2  resCollectTwo ">
+              <div className="p-2 pt-4 ps-3">
+                <p className="mb-0 text-white">HOT LIST</p>
+                <h4 className="mb-0 text-white">Men Collection</h4>
+                <p className="text-white">Shop now</p>
+              </div>
+            </div>
+            <div className="col-12  mb-2 resCollectthree">
+              <div className="p-2 pt-4 ps-3">
+                <p className="mb-0 text-white">HOT LIST</p>
+                <h4 className="mb-0 text-white">Kids Collection</h4>
+                <p className="text-white">Shop now</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </Fragment>
   );
--- src/navbar.js
+++ src/navbar.js
@@ -24,23 +24,44 @@ export function Navbar() {
     <Fragment>
       <div className="navMain border">
         <div className="container">
-          <div className="row py-4">
-            <div className="col-6">logo</div>
+          <div className="row py-3">
             <div className="col-6">
-              <div className="navLinkWarp">
-                <div>
+               
+              <img src="images/icons/tylorgang.png" style={{width:"60px"}}/> 
+            </div>
+            <div className="col-6">
+              <div className="navlinkMain d-flex justify-content-end">
+                <div className="navlinks">
                   <a href="" onClick={(e) => handleSearch(e)}>
-                    <img src="images/icons/search.png" className="Navicon" />
+                    <img src="images/icons/search.png" className="searchicn" />
+                  </a>
+                  <a>
+                    <img src="images/icons/cart.png" className="carticn mx-4" />
+                  </a>
+                  <a>
+                    <img src="images/icons/user.png" className="usericn" />
                   </a>
                 </div>
-                <div className="mx-4">
-                  <img
-                    src="images/icons/shopping-bag.png"
-                    className="Navicon"
-                  />
+
+                <div>
+                  <a onClick={(e) => handleSearch(e)}>
+                    <img
+                      src="images/icons/search.png"
+                      className="searchicn resicons  me-3 mt-1"
+                    />
+                  </a>
                 </div>
-                <div className="ms-3">
-                  <img src="images/icons/user.png" className="Navicon" />
+                <div>
+                  <a>
+                    <img
+                      src="images/icons/cart.png"
+                      className="carticn resicons"
+                    />
+                  </a>
+                </div>
+
+                <div className="">
+                  <img src="images/icons/menu.png" className="menuImg" />
                 </div>
               </div>
             </div>
@@ -55,12 +76,14 @@ export function Navbar() {
             className="searchInput "
             placeholder="Search here..."
           />
-          <div className={`${xmark ? "xmarkActive" : "xmarkInactive "} xmark`}>
+          <div className={`${xmark ? "xmarkActive" : "xmarkInactive "} xmark `}>
             <a href="" onClick={(e) => handleXmark(e)}>
-              <img src="images/icons/x-mark.png" className="xmarkimg" />{" "}
+              <img
+                src="images/icons/x-mark.png"
+                className="xmarkimg xmarkActiveres"
+              />
             </a>
           </div>
-          
         </div>
       </div>
     </Fragment>
